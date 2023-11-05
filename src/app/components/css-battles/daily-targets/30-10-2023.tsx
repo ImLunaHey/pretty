80bec91389d460315d0866a9f8851fb15ce899d3
luna <luna@wvvw.me>
2023-11-05 14:25:25 +1030

    feat: daily-targets/30-10-2023

---
+++ src/app/components/css-battles/daily-targets/30-10-2023.tsx
@@ -0,0 +1,13 @@
+export const DailyTargets_30102023 = () => {
+  return (
+    <div className="bg-[#F58220] w-screen h-screen">
+      <div className="bg-[#FFFFFF] absolute w-[280px] h-[180px] top-[60px] left-[60px] rounded-[200px]" />
+      <div className="bg-[#F58220] absolute w-[220px] h-[120px] top-[90px] left-[90px] rounded-[200px]" />
+      <div className="bg-[#FFFFFF] absolute w-[160px] h-[60px] top-[120px] left-[120px] rounded-[200px]" />
+      <div className="bg-[#F58220] absolute w-[30px] h-[30px] top-[60px] left-[185px]" />
+      <div className="bg-[#FFFFFF] absolute w-[30px] h-[30px] top-[90px] left-[185px]" />
+      <div className="bg-[#FFFFFF] absolute w-[30px] h-[30px] top-[180px] left-[185px]" />
+      <div className="bg-[#F58220] absolute w-[30px] h-[30px] top-[210px] left-[185px]" />
+    </div>
+  );
+};
