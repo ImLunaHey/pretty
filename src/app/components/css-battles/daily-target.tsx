4bf2ed48d20320bc5a3d71bc8f8152cf265ce62b
luna <luna@wvvw.me>
2023-11-05 18:00:24 +1030

    feat: daily-targets/28-07-2023

--- src/app/components/css-battles/daily-target.tsx
+++ src/app/components/css-battles/daily-target.tsx
@@ -2,6 +2,7 @@ import { DailyTargets_01112023 } from "./daily-targets/01-11-2023";
 import { DailyTargets_02112023 } from "./daily-targets/02-11-2023";
 import { DailyTargets_03112023 } from "./daily-targets/03-11-2023";
 import { DailyTargets_04112023 } from "./daily-targets/04-11-2023";
+import { DailyTargets_28072023 } from "./daily-targets/28-07-2023";
 import { DailyTargets_30102023 } from "./daily-targets/30-10-2023";
 import { DailyTargets_31102023 } from "./daily-targets/31-10-2023";
 
@@ -28,6 +29,10 @@ type Battle = {
 };
 
 export const battles = [
+  {
+    href: "https://cssbattle.dev/play/xVZiXaTbquOO8zacDDjt",
+    component: DailyTargets_28072023,
+  },
   {
     href: "https://cssbattle.dev/play/QGHPRtNWxdMMzt5zo4lj",
     component: DailyTargets_30102023,
---
+++ src/app/components/css-battles/daily-targets/28-07-2023.tsx
@@ -0,0 +1,21 @@
+export const DailyTargets_28072023 = () => {
+  return (
+    <div className="bg-[#F7EC7D] relative h-screen w-screen">
+      <div className="w-[400px] h-[300px] justify-center">
+        <div className="absolute bg-[#62306D] w-screen h-[50%] bottom-0 left-0 right-0" />
+        <div
+          className="absolute bg-[#62306D] w-[150px] h-[150px] left-0 rounded-full"
+          style={{ top: "calc(50% - 75px)" }}
+        />
+        <div
+          className="absolute bg-[#F7EC7D] h-[100px] left-[150px] rounded-full"
+          style={{ width: "calc(100% - 300px)", top: "calc(50% - 50px)" }}
+        />
+        <div
+          className="absolute bg-[#62306D] w-[150px] h-[150px] right-0 rounded-full"
+          style={{ top: "calc(50% - 75px)" }}
+        />
+      </div>
+    </div>
+  );
+};
